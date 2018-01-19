8df653c499353e9570ccbad8ff9d8e30cd4cacfc
Eugene <hadanyonok@gmail.com>
2018-01-19 14:04:51 +0300

    catalog_imgs added

--- back/server/dao/sneakers.js
+++ back/server/dao/sneakers.js
@@ -16,10 +16,16 @@ const sequelize = new Sequelize('snkr_srch', 'postgres', 'admin', {
 });
 
 function get(filter) {
-  return sequelize.query(`SELECT brands.name as brand, sneakers.model, sneakers.id, currency, price, sizes.size
+  return sequelize.query(`SELECT sneaker_imgs.url as img, brands.name as brand, sneakers.model, sneakers.id, currency, price, sizes.size
     FROM sneakers.sneakers_to_stores
     LEFT JOIN sneakers.sneakers on sneaker_id = sneakers.id
     LEFT JOIN sneakers.brands on brand_id = brands.id
+    LEFT JOIN sneakers.sneaker_imgs on sneaker_imgs.sneaker_id = (
+      SELECT sneaker_imgs.sneaker_id 
+      FROM sneakers.sneaker_imgs
+      WHERE sneaker_imgs.sneaker_id = sneakers.id
+      LIMIT 1
+    )
     RIGHT JOIN sneakers.sizes on sneakers_to_stores.id = sizes.sneakers_to_stores_id`);
 }
 
